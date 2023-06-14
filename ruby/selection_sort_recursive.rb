908b4628ded628114d4f695c4857d581f6403f6b
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-06-14 10:38:09 -0400

    couldnt finish

--- ruby/selection_sort_recursive.rb
+++ ruby/selection_sort_recursive.rb
@@ -1,5 +1,15 @@
+# require "pry"
+
 def selection_sort_recursive(arr)
   # type your code in here
+  return [] if arr.empty?
+
+  min = arr.min
+  idx = arr.index(min)
+  arr.delete_at(idx)
+
+  result = selection_sort_recursive(arr)
+  result.unshift(min)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -13,4 +23,16 @@ if __FILE__ == $PROGRAM_NAME
 end
 
 # Please add your pseudocode to this file
+
+# return arr[0] if arr length == 1
+#  [2, 4]
+# arr[0] < selection_sort_recursive(arr[1..-1])
+#   return arr[0]
+#   else return selection_sort_recursive(arr[1..-1])
+# push into new array
+
 # And a written explanation of your solution
+# How do I recursively build an array
+#
+#
+#
